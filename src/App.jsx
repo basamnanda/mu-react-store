1cc13efc45740744204d978f8852d7027c9c00d5
basamnanda <basamnandaprabathreddy@gmail.com>
2025-06-11 11:28:44 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,33 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Login from "./Login";
 import Cart from "./Cart";
+import Logout from "./Logout";
+import Order from "./Order";
 import Register from "./Register";
+import Test from "./Test";
+import Product from "./Product";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 export const AppContext = createContext();
 function App() {
-  const [users,setUsers] = useState([])
-  const [cart,setCart] = useState({})
+  const [users, setUsers] = useState([]);
+  const [orders, setOrders] = useState([]);
+  const [cart, setCart] = useState({});
+  const [email, setEmail] = useState();
   return (
     <div>
-      <AppContext.Provider value={{users,setUsers,cart,setCart}}>
+      <AppContext.Provider
+        value={{ users, setUsers, cart, setCart, email, setEmail,orders,setOrders }}
+      >
         <BrowserRouter>
           <Header name="mu-react-store" />
           <Routes>
-            <Route index element={<Home />} />
-            <Route path="/" element={<Home />} />
+            <Route index element={<Test />} />
+            <Route path="/" element={<Test />} />
             <Route path="/cart" element={<Cart />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/logout" element={<Logout />} />
+            <Route path="/order" element={<Order />} />
             <Route path="/register" element={<Register />} />
           </Routes>
           <Footer />
